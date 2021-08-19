dac9a3d086a234aaa989cfdac98346eb9810d61a
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-08-19 16:49:21 +0200

    Finished InputOption component

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './Feed.css';
 import CreateIcon from '@material-ui/icons/Create';
+import ImageIcon from '@material-ui/icons/Image';
+import InputOption from './InputOption';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
 
 function Feed() {
     return (
@@ -14,9 +19,15 @@ function Feed() {
                     </form>
                 </div>
                 <div className="feed__inputOptions">
-                    <InputOption />
+                    <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+                    <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
+                    <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
+                    <InputOption Icon={CalendarViewDayIcon} title="Write article" color="#7FC15E" />
                 </div>
             </div>
+
+            {/* Posts */}
+
         </div>
     )
 }
