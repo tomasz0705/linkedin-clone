f4873396f975e4485ab831117e57ad5781b71cf3
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-08-19 16:23:05 +0200

    Added Feed JS & CSS

--- src/Feed.js
+++ src/Feed.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import './Feed.css';
+import CreateIcon from '@material-ui/icons/Create';
 
 function Feed() {
     return (
-        <div className="feed"></div>
+        <div className="feed">
+            <div className="feed__inputContainer">
+                <div className="feed__input">
+                    <CreateIcon />
+                    <form>
+                        <input type="text" />
+                        <button type="submit">Send</button>
+                    </form>
+                </div>
+                <div className="feed__inputOptions">
+                    <InputOption />
+                </div>
+            </div>
+        </div>
     )
 }
 
