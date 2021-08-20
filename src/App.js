5e45f4a58d53971c621e948a1edc3beb1ea2a5f4
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-08-20 12:38:21 +0200

    Updated userSlice & store js files of redux

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import './App.css';
+import { selectUser } from './features/userSlice';
 import Feed from './Feed';
 import Header from './Header';
 import Sidebar from './Sidebar';
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
       <Header />
 
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        {/* Widgets */}
-      </div>
-
+      {!user ? <Login /> : (
+              <div className="app__body">
+              <Sidebar />
+              <Feed />
+              {/* Widgets */}
+            </div>
+      )}
     </div>
   );
 }
--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -7,25 +7,23 @@ export const userSlice = createSlice({
   },
   // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
-    increment: (state) => {
+    login: (state, action) => {
       // Redux Toolkit allows us to write "mutating" logic in reducers. It
       // doesn't actually mutate the state because it uses the Immer library,
       // which detects changes to a "draft state" and produces a brand new
       // immutable state based off those changes
-      state.value += 1;
-    },
-    decrement: (state) => {
-      state.value -= 1;
+      state.value = action.payload;
     },
     // Use the PayloadAction type to declare the contents of `action.payload`
-    incrementByAmount: (state, action) => {
-      state.value += action.payload;
+    logout: (state) => {
+      state.user = null;
     },
   },
 });
 
-export const { increment, decrement, incrementByAmount } = userSlice.actions;
+export const { login, logout } = userSlice.actions;
 
+//Selectors
 export const selectUser = (state) => state.user.user;
 
 export default userSlice.reducer;
